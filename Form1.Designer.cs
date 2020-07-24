aea624c782bd45e417d920db3695e37a02142e8a
Yendouboame <aimewoagou12@gmail.com>
2020-07-24 11:21:51 +0000

    Application desktp basic Application_Menu.txt

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Application_Menu
+{
+    partial class FrmChoixMenu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbMenu = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnDecocherTout = new System.Windows.Forms.Button();
+            this.btnCommander = new System.Windows.Forms.Button();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.gbMenu.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbMenu
+            // 
+            this.gbMenu.Controls.Add(this.checkBox10);
+            this.gbMenu.Controls.Add(this.checkBox9);
+            this.gbMenu.Controls.Add(this.checkBox8);
+            this.gbMenu.Controls.Add(this.checkBox7);
+            this.gbMenu.Controls.Add(this.checkBox6);
+            this.gbMenu.Controls.Add(this.checkBox5);
+            this.gbMenu.Controls.Add(this.checkBox4);
+            this.gbMenu.Controls.Add(this.checkBox3);
+            this.gbMenu.Controls.Add(this.checkBox2);
+            this.gbMenu.Controls.Add(this.checkBox1);
+            this.gbMenu.Location = new System.Drawing.Point(0, 0);
+            this.gbMenu.Name = "gbMenu";
+            this.gbMenu.Size = new System.Drawing.Size(361, 168);
+            this.gbMenu.TabIndex = 0;
+            this.gbMenu.TabStop = false;
+            this.gbMenu.Text = "Menu";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnDecocherTout);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 174);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(307, 87);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cocher tout";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDecocherTout
+            // 
+            this.btnDecocherTout.Location = new System.Drawing.Point(208, 21);
+            this.btnDecocherTout.Name = "btnDecocherTout";
+            this.btnDecocherTout.Size = new System.Drawing.Size(72, 39);
+            this.btnDecocherTout.TabIndex = 1;
+            this.btnDecocherTout.Text = "Decocher tout";
+            this.btnDecocherTout.UseVisualStyleBackColor = true;
+            this.btnDecocherTout.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnCommander
+            // 
+            this.btnCommander.Location = new System.Drawing.Point(46, 314);
+            this.btnCommander.Name = "btnCommander";
+            this.btnCommander.Size = new System.Drawing.Size(72, 51);
+            this.btnCommander.TabIndex = 2;
+            this.btnCommander.Text = "Commander";
+            this.btnCommander.UseVisualStyleBackColor = true;
+            this.btnCommander.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnQuitter.Location = new System.Drawing.Point(220, 315);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(72, 50);
+            this.btnQuitter.TabIndex = 3;
+            this.btnQuitter.Text = "Quitez";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(57, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(57, 53);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(57, 87);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(57, 119);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(57, 145);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(80, 17);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(239, 29);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(80, 17);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "checkBox6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(239, 87);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(80, 17);
+            this.checkBox7.TabIndex = 6;
+            this.checkBox7.Text = "checkBox7";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(239, 53);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(80, 17);
+            this.checkBox8.TabIndex = 7;
+            this.checkBox8.Text = "checkBox8";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(239, 110);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(80, 17);
+            this.checkBox9.TabIndex = 8;
+            this.checkBox9.Text = "checkBox9";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(239, 145);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(86, 17);
+            this.checkBox10.TabIndex = 9;
+            this.checkBox10.Text = "checkBox10";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // FrmChoixMenu
+            // 
+            this.AcceptButton = this.btnCommander;
+            this.CancelButton = this.btnQuitter;
+            this.ClientSize = new System.Drawing.Size(436, 450);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.btnCommander);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.gbMenu);
+            this.Name = "FrmChoixMenu";
+            this.Text = "Menu";
+            this.gbMenu.ResumeLayout(false);
+            this.gbMenu.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbMenu;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnDecocherTout;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCommander;
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Application_Menu
+{
+    public partial class FrmChoixMenu : Form
+    {
+        public FrmChoixMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCocher_Click(object sender, EventArgs e)
+        {
+            foreach (CheckBox checkBox in gbMenu.Controls)
+            {
+                checkBox.Checked = true;
+            }
+        }
+
+        private void btnDecocherTout_Click(object sender, EventArgs e)
+        {
+            foreach (CheckBox checkBox in gbMenu.Controls)
+            {
+                checkBox.Checked = false;
+            }
+        }
+
+        private void btnCommander_Click(object sender, EventArgs e)
+        {
+            bool commandeVide = true;
+            StringBuilder sb = new StringBuilder();
+
+            foreach (CheckBox checkBox in gbMenu.Controls)
+            {
+                if (checkBox.Checked)
+                {
+                    sb.Append("\n- " + checkBox.Text);
+                    commandeVide = false;
+                }
+            }
+            if (commandeVide == false)
+                MessageBox.Show("Vous avez commandé :" + sb.ToString(), "Commande", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else MessageBox.Show("Vous n'avez rien commandé", "Commande", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
